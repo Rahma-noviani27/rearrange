b188c68b98dc09c4d7f6bc1ef73c99e4118e0d50
Rahma-noviani27 <rahmanoviani.rn@gmail.com>
2021-03-15 14:18:58 +0700

    Add more code for making format name

--- rearrange.py
+++ rearrange.py
@@ -1,1 +1,10 @@
+#!D:\rearrange\rearrange.py python
+'''this code for exercise using unit test'''
 
+import re
+
+def rearrange_name(name):
+    result = re.search(r"^([\w.]*), ([\w.]*)$", name)
+    if result is None:
+        return ""
+    return "{} {}".format (result[2], result[1])

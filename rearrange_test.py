ca4a6ea8fa511c9f59bea879356d880e292ac759
Rahma-noviani27 <rahmanoviani.rn@gmail.com>
2021-03-15 14:20:47 +0700

    Add more code for testing use unittest

--- rearrange_test.py
+++ rearrange_test.py
@@ -1,1 +1,17 @@
+#!D:\rearrange\rearrange_test.py python
 
+from rearrange import rearrange_name
+import unittest
+
+class TestRearrange(unittest.TestCase):
+    def test_basic(self):
+        testcase = "Lovelace, Ada"
+        expected = "Ada Lovelace"
+        self. assertEqual(rearrange_name(testcase),expected)
+
+    def test_empty(self):
+        testcase = ""
+        expected =""
+        self. assertEqual(rearrange_name(testcase),expected)
+
+unittest.main()
